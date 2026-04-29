b77063d996ad79a8fb03be87d1bc3e7064a1d0da
24025lc <24025lc@hvhs.school.nz>
2026-04-29 00:17:48 +0000

    Apr 29, 2026, 12:17 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,6 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task: Task 3
+Put your header comment here!
+****************************/
+console.log("Running T3-maths.js")
+console.log("Hi my name is Liam Chung")
