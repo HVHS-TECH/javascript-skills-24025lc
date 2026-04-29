9c4efaf9566f5293dadb8210a6333d544ff3ae83
24025lc <24025lc@hvhs.school.nz>
2026-04-29 00:02:51 +0000

    Apr 29, 2026, 12:02 AM

--- t02_variables.js
+++ t02_variables.js
@@ -3,9 +3,21 @@ Name of task: Task 2
 Put your header comment here!
 ****************************/
 console.log("Running T2-variables.js")
+console.log("Hi my name is Liam Chung")
 
 //Variables
-console.log(/*Pocket Money*/)
+var birth_year = 2010
+var name = "Liam"
+var age = 15; 
+var pocketMoney = 200 
+var current_year = 2026
+
+console.log("Pocket Money")
+console.log("Running t02_variables")
+console.log ("As of"+current_year+"you are"+age+ " years old" )
+console.log ("You have"+pocketMoney+ "dollars")
+
+
 
 
 
@@ -14,12 +26,15 @@ console.log(/*Pocket Money*/)
 
  let pocketMoney = 3 
  console.log(pocketMoney)
-
+ 
 
 
  *****************************/
 
-
+console.log("Pocket Money")
+console.log("Running t02_variables")
+console.log ("As of"+current_year+ "you are"+age+ " years old" )
+console.log ("You have"+pocketMoney+ "dollars")
 
 
  /****************************
