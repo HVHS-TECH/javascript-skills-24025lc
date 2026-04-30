22e29cca657e423b7f3d10d101678c2b7bbb9e0e
24025lc <24025lc@hvhs.school.nz>
2026-04-30 00:51:15 +0000

    Apr 30, 2026, 12:51 AM

--- t03_maths.js
+++ t03_maths.js
@@ -12,7 +12,10 @@ var age = 15;
 var pocketMoney = 200 
 var current_year = 2026
 
-
+console.log("Pocket Money")
+console.log("Running t02_variables")
+console.log ("As of "+current_year+ " you are "+age+ " years old " )
+console.log ("You have "+ pocketMoney +  " dollars")
 
 console.log("add")
 
