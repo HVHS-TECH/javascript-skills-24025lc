66e22446f98c482574a50fe15dda9c9f59f857fe
24025lc <24025lc@hvhs.school.nz>
2026-04-30 01:10:04 +0000

    Apr 30, 2026, 1:10 AM

--- t03_maths.js
+++ t03_maths.js
@@ -9,15 +9,19 @@ console.log("Hi my name is Liam Chung")
 var user = "user"
 var birth_year = 2010
 var name = "Liam"
-var age = 15; 
+var age = 16; 
 var pocketMoney = 200 
 var current_year = 2026
+var future_me = 10
+var half_money = 100
+var extra_money = 103
 
 
 var birth_year = 0
 birth_year = current_year - age;
 
-
+var future_me_2 = 0
+future_me_2 = age + future_me
 
 
 console.log("Pocket Money")
@@ -30,4 +34,8 @@ console.log ("You have "+ pocketMoney +  " dollars")
 
 console.log("Hi "+ user)
 console.log ("As of "+current_year+ " you are "+age+ " years old " )
-console.log ("You were born in"+ birth_year)
+console.log ("You were born in "+ birth_year )
+console.log ("In 10 years you will be "+ future_me_2) 
+console.log ("You have "+ pocketMoney + " dollars ")
+console.log ("You spend half of your money, now you have "+ half_money + " dollars")
+console.log ("Then you get $3, now you have "+ extra_money+ " dollars")
