b66efa666d8ad51fb1dd069fb7d22d9bd22ab2af
24025lc <24025lc@hvhs.school.nz>
2026-04-30 01:26:53 +0000

    Apr 30, 2026, 1:26 AM

--- t03_maths.js
+++ t03_maths.js
@@ -39,3 +39,8 @@ console.log ("In 10 years you will be "+ future_me_2)
 console.log ("You have "+ pocketMoney + " dollars ")
 console.log ("You spend half of your money, now you have "+ half_money + " dollars")
 console.log ("Then you get $3, now you have "+ extra_money+ " dollars")
+
+
+
+
+
