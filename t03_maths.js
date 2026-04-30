e89af274b202a8a68909bf9c139b4479c88be0d1
24025lc <24025lc@hvhs.school.nz>
2026-04-30 00:56:05 +0000

    Apr 30, 2026, 12:56 AM

--- t03_maths.js
+++ t03_maths.js
@@ -6,16 +6,24 @@ console.log("Running T3-maths.js")
 console.log("Hi my name is Liam Chung")
 
 //Variables
+var user = "user"
 var birth_year = 2010
 var name = "Liam"
 var age = 15; 
 var pocketMoney = 200 
 var current_year = 2026
 
+
+
+
 console.log("Pocket Money")
 console.log("Running t02_variables")
 console.log ("As of "+current_year+ " you are "+age+ " years old " )
 console.log ("You have "+ pocketMoney +  " dollars")
 
-console.log("add")
 
+/*Task 3 ****************************/
+
+console.log("Hi "+ user)
+console.log ("As of "+current_year+ " you are "+age+ " years old " )
+console.log ("You were born in " )
