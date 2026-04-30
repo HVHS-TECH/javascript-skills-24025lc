c9f0e21c6e16a57f021c90c6b49eb3f1cb3daf89
24025lc <24025lc@hvhs.school.nz>
2026-04-30 00:50:29 +0000

    Apr 30, 2026, 12:50 AM

--- t03_maths.js
+++ t03_maths.js
@@ -4,3 +4,15 @@ Put your header comment here!
 ****************************/
 console.log("Running T3-maths.js")
 console.log("Hi my name is Liam Chung")
+
+//Variables
+var birth_year = 2010
+var name = "Liam"
+var age = 15; 
+var pocketMoney = 200 
+var current_year = 2026
+
+
+
+console.log("add")
+
