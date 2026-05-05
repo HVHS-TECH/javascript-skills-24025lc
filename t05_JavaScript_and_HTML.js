c4df98765a49bd3dab23490bb94e1750855d94a5
24025lc <24025lc@hvhs.school.nz>
2026-05-05 01:07:53 +0000

    May 5, 2026, 1:07 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,42 @@
-/* Paste your code from task 3, Maths here */
+console.log("Running T3-maths.js")
+console.log("Hi my name is Liam Chung")
+
+//Variables
+var user = "user"
+var birth_year = 2010
+var name = "Liam"
+var age = 16; 
+var pocketMoney = 200 
+var current_year = 2026
+var future_me = 10
+var half_money = 100
+var extra_money = 103
+
+
+var birth_year = 0
+birth_year = current_year - age;
+
+var future_me_2 = 0
+future_me_2 = age + future_me
+
+
+console.log("Pocket Money")
+console.log("Running t02_variables")
+console.log ("As of "+current_year+ " you are "+age+ " years old " )
+console.log ("You have "+ pocketMoney +  " dollars")
+
+
+/*Task 3 ****************************/
+
+console.log("Hi "+ user)
+console.log ("As of "+current_year+ " you are "+age+ " years old " )
+console.log ("You were born in "+ birth_year )
+console.log ("In 10 years you will be "+ future_me_2) 
+console.log ("You have "+ pocketMoney + " dollars ")
+console.log ("You spend half of your money, now you have "+ half_money + " dollars")
+console.log ("Then you get $3, now you have "+ extra_money+ " dollars")
+
+
+
+
+
