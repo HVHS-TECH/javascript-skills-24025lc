0e57cfdf8ebfd0a6c2293a0bef147758fd784a04
24025lc <24025lc@hvhs.school.nz>
2026-05-18 21:15:53 +0000

    May 18, 2026, 9:15 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -20,10 +20,7 @@ var future_me_2 = 0
 future_me_2 = age + future_me
 
 
-console.log("Pocket Money")
-console.log("Running t02_variables")
-console.log ("As of "+current_year+ " you are "+age+ " years old " )
-console.log ("You have "+ pocketMoney +  " dollars")
+
 
 
 /*Task 3 ****************************/
@@ -35,7 +32,10 @@ console.log ("In 10 years you will be "+ future_me_2)
 console.log ("You have "+ pocketMoney + " dollars ")
 console.log ("You spend half of your money, now you have "+ half_money + " dollars")
 console.log ("Then you get $3, now you have "+ extra_money+ " dollars")
-
+console.log("Pocket Money")
+console.log("Running t02_variables")
+console.log ("As of "+current_year+ " you are "+age+ " years old " )
+console.log ("You have "+ pocketMoney +  " dollars")
 
 
 
