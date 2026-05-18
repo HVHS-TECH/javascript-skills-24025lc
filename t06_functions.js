92011ac29fc587235ebe8f8b3afee3efbc5533fb
24025lc <24025lc@hvhs.school.nz>
2026-05-18 21:20:27 +0000

    May 18, 2026, 9:20 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,9 @@
-/* Paste your code from task 5 here */
+console.log("Hi "+ user)
+console.log ("As of "+current_year+ " you are "+age+ " years old " )
+console.log ("You were born in "+ birth_year )
+console.log ("In 10 years you will be "+ future_me_2) 
+console.log ("You have "+ pocketMoney + " dollars ")
+console.log ("You spend half of your money, now you have "+ half_money + " dollars")
+console.log ("Then you get $3, now you have "+ extra_money+ " dollars")
+console.log("Pocket Money")
+
