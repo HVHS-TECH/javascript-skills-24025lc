c11842699d9ba8323308f9f8f24c6a12c133438f
24025lc <24025lc@hvhs.school.nz>
2026-05-18 21:50:46 +0000

    May 18, 2026, 9:50 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,26 @@
-/* Paste your code from task 6 here */
+/****************************
+Name of task: Task 7
+Functions and Parameters
+****************************/
+console.log("Running T7-parameters.js")
+console.log("Hi my name is Liam Chung")
+
+
+
+
+/****************************
+Functions
+****************************/
+function displwelcome(){
+  // Change OUTPUT to match your HTML ID exactly
+  let output = document.getElementById("spaceForJavaScriptOutput");
+  output.innerHTML += "<p>Welcome to the Shop</p>";
+}
+
+
+
+
+/****************************
+Main Code
+****************************/
+welcome() 
