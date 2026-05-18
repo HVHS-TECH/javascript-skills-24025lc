085e8c8b5f7c2f0e87c5d9ea0d0ec47eb56f96bd
24025lc <24025lc@hvhs.school.nz>
2026-05-18 21:45:30 +0000

    May 18, 2026, 9:45 PM

--- t06_functions.js
+++ t06_functions.js
@@ -11,19 +11,17 @@ console.log("Hi my name is Liam Chung")
 /****************************
 Functions
 ****************************/
-function welcome(){"to "+ the + shop
+function welcome(){
   // Add a line to the html page
   OUTPUT.innerHTML += "<p>Welcome to the Shop</p>";
 }
 
-//Variables
-var to = "to"
-var the ="the"
-var shop ="shop"
+
 
 
 
 /****************************
 Main Code
 ****************************/
-welcome("to "+ the + shop)
+welcome() {OUTPUT.innerHTML += "<p>Welcome to the Shop</p>";
+}
