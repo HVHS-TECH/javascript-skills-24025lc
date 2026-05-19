8b896c0779aa0531bd98d4c97101925d2b136f68
24025lc <24025lc@hvhs.school.nz>
2026-05-19 07:05:24 +0000

    May 19, 2026, 7:05 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -2,10 +2,18 @@
 Name of task: Task 12
 Conditionals
 ****************************/
-console.log("Running T11_data_types.js")
+console.log("Running T12_conditionals.js")
 console.log("Hi my name is Liam Chung")
 
 
+/****************************
+Functions
+****************************/
+
+function getFormInput(){
+  const NAME_FIELD = document.getElementById("nameField");
+  let userName = NAME_FIELD.value;
+}
 
 
 
@@ -24,4 +32,29 @@ function handleUserData() {
   
   // 3. Format the money value neatly to two decimal places
   let formattedMoney = Number(pocketMoney).toFixed(2);
+}
+
+
+/*Task 12 ****************************/
+
+function handleUserData() {
+  // 1. Get references to the HTML elements
+  const AGE_FIELD = document.getElementById("ageField");
+  const MONEY_FIELD = document.getElementById("moneyField");
+  const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+  
+  // 2. Extract the values from the inputs
+  let userAge = Number(AGE_FIELD.value);
+  let pocketMoney = Number(MONEY_FIELD.value);
+  
+  // 3. Conditional Logic for the chocolate bar
+  let message = "A chocolate bar costs $4<br><br>";
+
+  if (pocketMoney >= 4) {
+    message += "You CAN afford a chocolate bar";
+  } else {
+    message += "Sorry you CAN'T afford a chocolate bar";
+  }
+
+
 }
