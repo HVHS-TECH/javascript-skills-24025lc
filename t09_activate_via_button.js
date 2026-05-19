e149e21bd6eab072779e608c33bc6ec8df63217d
24025lc <24025lc@hvhs.school.nz>
2026-05-19 03:44:32 +0000

    May 19, 2026, 3:44 AM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,34 @@
-/* Paste your code from task 7 here*/
+/****************************
+Name of task: Task 9
+Active Via Button
+****************************/
+console.log("Running T9-active_via_button.js")
+console.log("Hi my name is Liam Chung")
+
+
+
+
+/****************************
+Functions
+****************************/
+function displaywelcome(_name, _price){
+  // Change OUTPUT to match your HTML ID exactly
+  let output = document.getElementById("spaceForJavaScriptOutput");
+  output.innerHTML += "<p>Chocolate Bar "+_price+"</p>";
+  output.innerHTML += "<p>Chips "+_price+"</p>";
+  output.innerHTML += "<p>Drink "+_price+"</p>";
+
+}
+
+
+
+/****************************
+Main Code
+****************************/
+displayWelcome("Chocolate bar",4)
+
+
+
+/*Task 9 ****************************/
+
+
