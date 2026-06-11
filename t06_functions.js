55721d04c645a04abc7e1472d5184c74153bcaaa
24025lc <24025lc@hvhs.school.nz>
2026-06-11 04:31:11 +0000

    Jun 11, 2026, 4:31 AM

--- t06_functions.js
+++ t06_functions.js
@@ -12,7 +12,6 @@ console.log("Hi my name is Liam Chung")
 Functions
 ****************************/
 function welcome(){
-  // Change OUTPUT to match your HTML ID exactly
   let output = document.getElementById("spaceForJavaScriptOutput");
   output.innerHTML += "<p>Welcome to the Shop</p>";
 }
